b7eadaa9af7465716f0fbecdd93a95019b412588
pierrick <p.meignen@free.fr>
2026-05-31 21:53:57 +0200

    ajout de EUCLID et nettoyage fin des fichiers

--- tests/EUCLID.py
+++ tests/EUCLID.py
@@ -11,25 +11,25 @@ from utils import *
 def init():
     go_to_page(TIME)
     set_parameter_value(1, 127) # clock EXT
-    go_to_page(DELAY)
-    set_parameter_value(1, 8) # CHANNEL OUT 1
+    go_to_page(EUCLID)
+    set_parameter_value(1, 30) # length = 4
     set_parameter_value(2, 8) # CHANNEL OUT 1
-    set_parameter_value(3, 100) # repeat = 2
-    set_parameter_value(4, 77) # 75itch = 2
-    set_parameter_value(5, 70) # sync = 1 / 2    
+    set_parameter_value(3, 75) # min = C5
+    set_parameter_value(4, 57) # max = C4
+    set_parameter_value(5, 44) # 1 beat
+    set_parameter_value(6, 61) # 1 beat
+    set_parameter_value(8, 40) # shift = 1
 
 def test(clock):
     print("************ test clock", clock)
+    F8_count = 24
     if clock == "interne":
         go_to_page(TIME)
         set_parameter_value(1, 0) # clock INT
         set_parameter_value(3, 0) # BPM 30
-        go_to_page(DELAY)
+        go_to_page(EUCLID)
         restart()
-        midi_out.send_message([144, 60, 100]) # note on
-        time.sleep(3 * delay)
-        midi_out.send_message([128, 60, 0]) # note off
-        time.sleep(33 * delay)
+        time.sleep(F8_count * delay)
     elif clock == "externe":
         go_to_page(TIME)
         set_parameter_value(1, 127) # clock EXT
@@ -37,10 +37,7 @@ def test(clock):
         # on lance une séquence complète
         midi_out.send_message([0xFA]) # début
         time.sleep(delay)
-        midi_out.send_message([144, 60, 100]) # note on
-        send_pulse(delay, 3)
-        midi_out.send_message([128, 60, 0]) # note off
-        send_pulse(delay, 33)
+        send_pulse(delay, F8_count)
         midi_out.send_message([0xFC]) # fin
         time.sleep(delay)
 
